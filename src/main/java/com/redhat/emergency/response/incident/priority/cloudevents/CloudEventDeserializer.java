72beef873e94bbc0094d732cb9c2b2e8c1a4a53e
Bernard Tison <bernard.tison@gmail.com>
2021-04-20 14:56:45 +0200

    Handling of cloudevent messages produced by process service outbox
    Note: configuring the mapper through Kafka config doesn't work, as the vert.x client only accepts string as configuration value, not an object.

--- src/main/java/com/redhat/emergency/response/incident/priority/cloudevents/CloudEventDeserializer.java
+++ src/main/java/com/redhat/emergency/response/incident/priority/cloudevents/CloudEventDeserializer.java
@@ -3,6 +3,10 @@ package com.redhat.emergency.response.incident.priority.cloudevents;
 import java.util.Map;
 
 import io.cloudevents.CloudEvent;
+import io.cloudevents.CloudEventData;
+import io.cloudevents.core.message.MessageReader;
+import io.cloudevents.kafka.KafkaMessageFactory;
+import io.cloudevents.rw.CloudEventDataMapper;
 import org.apache.kafka.common.header.Headers;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.slf4j.Logger;
@@ -12,26 +16,27 @@ public class CloudEventDeserializer implements Deserializer<CloudEvent> {
 
     private static Logger log = LoggerFactory.getLogger(CloudEventDeserializer.class);
 
-    private io.cloudevents.kafka.CloudEventDeserializer delegate;
+    private final CloudEventDataMapper<CloudEventData> mapper;
 
     public CloudEventDeserializer() {
-        delegate = new io.cloudevents.kafka.CloudEventDeserializer();
+        mapper = new CloudEventQuotedStringDataMapper();
     }
 
     @Override
     public void configure(Map<String, ?> configs, boolean isKey) {
-        delegate.configure(configs, isKey);
+
     }
 
     @Override
     public CloudEvent deserialize(String topic, byte[] data) {
-        return delegate.deserialize(topic, data);
+        throw new UnsupportedOperationException("CloudEventDeserializer supports only the signature deserialize(String, Headers, byte[])");
     }
 
     @Override
     public CloudEvent deserialize(String topic, Headers headers, byte[] data) {
         try {
-            return delegate.deserialize(topic, headers, data);
+            MessageReader reader = KafkaMessageFactory.createReader(headers, data);
+            return reader.toEvent(mapper);
         } catch (Exception e) {
             log.error("Error deserializing CloudEvent", e);
         }
